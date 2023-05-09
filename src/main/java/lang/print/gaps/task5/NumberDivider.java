4c3a1a261048623f88e08d31500256f585477a45
MKN2003 <kazimovmiraziz197@gmail.com>
2023-05-09 19:20:38 +0500

    changes have been made.

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+    void divide(float toBeDivided) {
+        toBeDivided = 19;
+        toBeDivided /= 5;
+
+        System.out.println(toBeDivided);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        minutes = 11.5F;
+        float seconds = 60.0F;
+        minutes *= seconds;
+
+        System.out.println(minutes);
     }
 }
